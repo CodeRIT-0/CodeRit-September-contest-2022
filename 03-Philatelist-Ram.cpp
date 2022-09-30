c2ec7f077484c2c6fc7671123d55a7b44ed94f87
Ninaad <78657749+ninaadps@users.noreply.github.com>
2022-09-30 16:19:51 +0530

    Create 03-Philatelist-Ram.cpp

---
+++ 03-Philatelist-Ram.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    ll stones,present;
+        cin>>stones;
+        present=0;
+        present=2*(stons/3);
+        if(stones%3>0)
+            present++;
+        cout<<present<<endl;
+    
+    return 0;
+}
